dcb0cd09d89a0c2d26817f797970a22a759a1f7f
thegamebegins25 <wdppostcard@gmail.com>
2023-11-21 08:54:45 -0500

    extremely basic bot, with image generation

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # geneplore-self-hosted
 Geneplore AI's Discord bot that you can host yourself.
+
+To run, follow these steps:
+
+1. Install Python 3.8 or higher.
+2. Install the required packages with `pip install -r bot/requirements.txt`.
+3. Create a Discord bot by following step 1 at https://discord.com/developers/docs/getting-started - When you invite the bot to your server, make sure both `applications.commands` and `bot` are checked. Also, select Administrator under Bot Permissions to avoid any errors.
+4. Enable the Message Content intent in the Bot tab.
+5. Copy the bot token and paste it in the `DISCORD_TOKEN` variable in `bot/bot.py`, or use environmental variables with `os.getenv()`.
+6. Get your Geneplore API token by going to our [Discord server](https://geneplore.com/discord) and typing `/api` in the #gpt-bot-1 channel. Then, select API Keys from the dropdown and click Create One. Copy this key, as it won't be shown again.
+7. Then, paste this key into the `GENEPLORE_KEY` variable in `bot/bot.py`, or use environmental variables with `os.getenv()`.
+8. Run the bot with `python bot/bot.py`, or select Run and Debug in your favorite IDE.
+9. Ensure your bot is online, in your server, and ready to use. If the bot is offline, check the console for any errors. If the bot is online but no commands appear, type `!sync` in a DM with the bot to sync the commands. It might take a few minutes, but be patient. Feel free to ask us in our [Discord server](https://geneplore.com/discord) if you need any help.
---
+++ bot/bot.py
@@ -0,0 +1,97 @@
+import discord
+from discord import app_commands
+from discord.app_commands import Choice
+import os
+import aiohttp
+import json
+import asyncio
+import base64
+
+DISCORD_TOKEN = "MTE3NjMyMjQzMTkwNDkwNzM5Ng.GBLhCy.l6KXDBf8bWyHwf4jmSkKj4Q2_vqN8tsda6fuko"
+GENEPLORE_KEY = "gp-_XzRyrtgaMTna7xCUAuMqt545wqpTJ0pMq27iHx_7wcuimZtVpCefTO8GMd29W6CCYhIpRcPtwh_JbV075wg2g"
+
+
+#Initialize bot with intents and command tree
+intents = discord.Intents.default()
+intents.messages = True
+intents.message_content = True
+
+client = discord.AutoShardedClient(intents=intents)
+
+tree = discord.app_commands.CommandTree(client=client)
+client.tree = tree
+
+#Headers for all calls to the Geneplore API
+geneplore_headers = {
+    "accept": "application/json",
+    "content-type": "application/json",
+    "authorization": GENEPLORE_KEY
+}
+
+#Embed functions
+async def ErrorEmbed(error, message_id, user_id, command, title = "Unknown Error"):
+    embed = discord.Embed(title=title, description=error + "\n\nNeed support? Join our support server at https://geneplore.com/discord", color=discord.Color.red())
+    return embed
+
+
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    if message.content == "!sync":
+        print("Syncing...")
+        sync = await client.tree.sync()
+        print(sync)
+        await message.reply("Synced!")
+        return
+
+@tree.command(name="image", description="Generate an image from a prompt.")
+@app_commands.describe(model='Choose the image generation model.')
+@app_commands.choices(model=[
+    Choice(name='Stable Diffusion XL v1.0', value="stable-diffusion-xl-1024-v1-0"),
+    Choice(name='DALL-E 3', value="dall-e-3"),
+    Choice(name='DALL-E 2', value="dall-e-2"),
+    Choice(name='Anything v4', value="sd-anything-v4"),
+    Choice(name='OpenJourney', value="sd-openjourney"),
+    Choice(name='Kandinsky v2.1', value="kandinsky-v2")
+])
+async def image(interaction: discord.Interaction, model: Choice[str], prompt: str):
+        await interaction.response.send_message("Generating image...")
+        session = aiohttp.ClientSession()
+        
+        widths = {
+            "stable-diffusion-xl-1024-v1-0": 1024,
+            "dall-e-3": 1024,
+            "dall-e-2": 1024,
+            "sd-anything-v4": 512,
+            "sd-openjourney": 512,
+            "kandinsky-v2": 512
+        }
+
+        print(prompt)
+
+        data = {
+            "prompt": prompt,
+            "model": model.value,
+            "width": widths[model.value],
+        }
+        
+        req = await session.post("https://api.geneplore.com/image/generate", headers=geneplore_headers, data=json.dumps(data))
+
+        resp = await req.json()
+        b64 = resp.get("base64")
+        await session.close()
+        if not b64:
+            await interaction.edit_original_response(content=None, embed=await ErrorEmbed(str(resp), interaction.id, interaction.user.id, interaction.command.name))
+            return
+        
+        image = base64.b64decode(b64)
+        with open(str(interaction.id) + ".png", "wb") as f:
+            f.write(image)
+
+        await interaction.edit_original_response(content=interaction.user.mention + "\n" + prompt, attachments=[discord.File(str(interaction.id) + ".png")])
+
+        os.remove(str(interaction.id) + ".png")
+        return
+
+client.run(DISCORD_TOKEN)
